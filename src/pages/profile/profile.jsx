dafcc86659369ec9f40bfe45486b425357f04541
InFrantic <rakin.vladislav@yandex.ru>
2023-11-24 22:04:51 +0300

    update

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import styles from './profile.module.css';
 import { Button, EmailInput, Input, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components'
 import { useDispatch, useSelector } from 'react-redux';
